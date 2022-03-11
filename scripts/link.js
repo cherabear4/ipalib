125cb4c5a13e1b016378abea5f481f3c395047cc
piggydoe <67165527+piggydoe@users.noreply.github.com>
2022-03-10 18:09:24 -0800

    Add files via upload

---
+++ scripts/link.js
@@ -0,0 +1,6 @@
+function ttv() {
+    window.location.href="https://twitch.tv/osutabletuser";
+}
+function yt(){
+    window.location.href="https://www.youtube.com/channel/UCI_mIGGWwlCWwh2D0R3if1A";
+}
